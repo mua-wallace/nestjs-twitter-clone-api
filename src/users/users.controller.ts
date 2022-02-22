a9c52bc296131430f03eb94559251a6ff7081731
mua-wallace <muawallizewi@gmail.com>
2022-02-22 18:12:21 +0100

    added user routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Put } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('/@:username')
+  getUserByUsername(@Param() param): string {
+    // TODO: add actual logic
+    return `details of username = ${param.username}`;
+  }
+  @Get('/:userid')
+  getUserByUserId(@Param() param): string {
+    // TODO: add actual logic
+    return `details of user id = ${param.userid}`;
+  }
+  @Patch('/:userId')
+  updateUserDetatails(@Param() param): string {
+    // TODO: add actual logic
+    return ` details of the user (id = ${param.userId}) updated`;
+  }
+
+  @Put('/:userid/follow')
+  followUser(): string {
+    // TODO: add actual logic
+    return 'you followed user';
+  }
+
+  @Delete('/:userid/follow')
+  unfollowUser(): string {
+    // TODO: add actual logic
+    return 'you unfollowed user';
+  }
+  @Get('/:userid/followers')
+  getFollowersOfUser(): string {
+    // TODO: add actual logic
+    return 'get followers of users';
+  }
+
+  @Put('/:userid/followees')
+  getFolloweesOfUser(): string {
+    // TODO: add actual logic
+    return 'get followees of given user';
+  }
+}
