5e8525154c68f7414dd9fb33ddf2e5eb1df21847
mua-wallace <muawallizewi@gmail.com>
2022-02-24 00:49:23 +0100

    swagger config added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('WalliceShareChat Api')
+    .setDescription('Api for sharing social ideas')
+    .setVersion('1.0')
+    .addTag('posts')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
