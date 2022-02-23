808cf3409b819f40f517238e54949acc95806d1f
mua-wallace <muawallizewi@gmail.com>
2022-02-24 00:48:18 +0100

    added db configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,29 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { PostsController } from './posts/posts.controller';
 import { HashtagsController } from './hashtags/hashtags.controller';
+import { UserEntity } from './users/users.entities';
+import { PostEntity } from './posts/posts.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UsersController, PostsController, HashtagsController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      username: 'wallice',
+      password: 'mbiketurah2020',
+      database: 'sharechatdb',
+      synchronize: true,
+      logger: 'advanced-console',
+      logging: 'all',
+      entities: [UserEntity, PostEntity],
+    }),
+  ],
+  controllers: [
+    AppController,
+    UsersController,
+    PostsController,
+    HashtagsController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
