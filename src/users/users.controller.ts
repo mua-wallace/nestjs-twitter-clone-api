f2e3363dc7b968bf76d5ecf099be763b53c4fd31
mua-wallace <muawallizewi@gmail.com>
2022-02-25 10:02:20 +0100

    addes actual logic for getUserById, createNewUser and UpdateUserDetails

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
@@ -8,9 +9,24 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
-import { NotFoundError } from 'rxjs';
+import { ApiProperty, ApiPropertyOptional, ApiTags } from '@nestjs/swagger';
+import { UserEntity } from './users.entities';
 import { UsersService } from './users.service';
+
+export class UserCreateRequestBody {
+  @ApiProperty() username: string;
+  @ApiProperty() password: string;
+  @ApiPropertyOptional() name: string;
+  @ApiPropertyOptional() bio: string;
+  @ApiPropertyOptional() avatar: string;
+}
+
+export class UserUpdateRequestBody {
+  @ApiProperty() password?: string;
+  @ApiPropertyOptional() name?: string;
+  @ApiPropertyOptional() bio?: string;
+  @ApiPropertyOptional() avatar?: string;
+}
 
 @ApiTags('users')
 @Controller('users')
@@ -18,29 +34,41 @@ export class UsersController {
   constructor(private userService: UsersService) {}
 
   @Get('/@:username')
-  async getUserByUsername(@Param('username') username: string): Promise<any> {
+  async getUserByUsername(
+    @Param('username') username: string,
+  ): Promise<UserEntity> {
     const user = await this.userService.getUserByUsername(username);
     if (!user) {
-      throw new NotFoundException('User not found');
+      throw new NotFoundException('User Not Found');
     }
     return user;
   }
 
   @Get('/:userid')
-  getUserByUserId(@Param('userid') userid: string): string {
-    // TODO: add actual logic
-    return `details of user id = ${userid}`;
+  async getUserByUserId(@Param('userid') userid: string): Promise<UserEntity> {
+    const user = await this.userService.getUserById(userid);
+    if (!user) {
+      throw new NotFoundException('User Not Found');
+    }
+    return user;
   }
 
   @Post('/')
-  createnewUser(): string {
-    return 'new user creted';
+  async createNewUser(
+    @Body() createUserRequest: UserCreateRequestBody,
+  ): Promise<UserEntity> {
+    const user = await this.userService.createUser(createUserRequest);
+    return user;
   }
 
+  // TODO: make sure that the user is authenticated as themselves
   @Patch('/:userid')
-  updateUserDetatails(@Param('userid') userid: string): string {
-    // TODO: add actual logic
-    return ` details of the user (id = ${userid}) updated`;
+  async updateUserDetatails(
+    @Param('userid') userid: string,
+    @Body() updateUserRequest: UserUpdateRequestBody,
+  ): Promise<UserEntity> {
+    const user = await this.userService.updateUser(userid, updateUserRequest);
+    return user;
   }
 
   @Put('/:userid/follow')
