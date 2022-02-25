b2188ce0c367bf9e792caf2031e8ae400546a659
mua-wallace <muawallizewi@gmail.com>
2022-02-25 10:05:42 +0100

    created getUserById, createUser, updateUser methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,55 @@
-import { Injectable } from '@nestjs/common';
-// import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable, Param } from '@nestjs/common';
 import { UsersRepository } from './user.repository';
 import { UserEntity } from './users.entities';
 
 @Injectable()
 export class UsersService {
   constructor(private usersRepository: UsersRepository) {}
-
+  /**
+   * @description find a user with given username
+   * @returns {Promise<UserEntity>}
+   */
   public async getUserByUsername(username: string): Promise<UserEntity> {
     return await this.usersRepository.findOne({ where: { username } });
+  }
+
+  /**
+   * @description find a user given userId
+   * @returns {Promise<UserEntity>} user if found
+   */
+
+  public async getUserById(userId: string): Promise<UserEntity> {
+    return await this.usersRepository.findOne({ where: { id: userId } });
+  }
+
+  /**
+   * @description create new user with given details
+   * @returns {Promise<UserEntity>} user if created
+   */
+
+  public async createUser(user: Partial<UserEntity>): Promise<UserEntity> {
+    return await this.usersRepository.save(user);
+  }
+
+  /**
+   * @description update a user with given details
+   * @returns {Promise<UserEntity>} user if updated
+   */
+
+  public async updateUser(
+    userId: string,
+    newUserDetails: Partial<UserEntity>,
+  ): Promise<UserEntity> {
+    const existingUser = await this.usersRepository.findOne({
+      where: { id: userId },
+    });
+    if (!existingUser) {
+      return null;
+    }
+    if (newUserDetails.bio) existingUser.bio = newUserDetails.bio;
+    if (newUserDetails.avatar) existingUser.avatar = newUserDetails.avatar;
+    if (newUserDetails.name) existingUser.name = newUserDetails.name;
+
+    return await this.usersRepository.save(existingUser);
   }
 }
