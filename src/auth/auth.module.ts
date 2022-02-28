f18c9623334cc1519a2c30bb5605f3dea1173f23
mua-wallace <muawallizewi@gmail.com>
2022-02-28 13:36:12 +0100

    created auth moudule

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { PasswordEntity } from './passwords.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([PasswordEntity])],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService],
+})
+export class AuthModule {}
